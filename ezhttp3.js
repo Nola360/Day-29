64709e152bff4dd81caddb1962ed36f44a777ed1
nola360 <adaramolajr@gmail.com>
2020-01-29 10:38:44 -0500

    added async await to get put post delete request project

---
+++ ezhttp3.js
@@ -0,0 +1,66 @@
+class EasyHTTP {
+  // Make http get request
+  async get(url) {
+    const response = await fetch(url)
+
+    const resData = await response.json();
+
+    return resData;
+  }
+
+
+  // Make Http Post request
+
+  async  post(url, data) {
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    const resData = await response.json();
+
+    return resData;
+
+  }
+
+  // PUT request
+  async put(url, data) {
+
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    const resData = await response.json();
+
+    return resData;
+
+
+  }
+
+  // Make an HTTP DELETE Request
+
+  async delete(url) {
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+    const resData = await 'Resource Deleted...';
+
+    return resData;
+
+
+  }
+
+}
+
